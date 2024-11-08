c1ab99429c44e14b65fbf11076e4bd75a2882de2
HoussamEddineSEHLAOUI <houssameddinesehlaoui@gmail.com>
2024-11-08 12:01:58 +0100

    first commit

--- app/components/ui/Intro.tsx
+++ app/components/ui/Intro.tsx
@@ -6,8 +6,8 @@ const Intro = () => {
       <div className='md:p-52 pt-40 pl-7'>
         <h5 className='md:mb-[2.5rem] mb-[1rem] text-xl md:text-4xl text-brand-secondary dark:text-slate-200 typography'>Hello, my name is</h5>
         <h1 className='mb-8 text-3xl md:text-5xl font-bold text-brand-primary dark:text-white typography'>Houssam Eddine Sehlaoui.</h1>
-        <h1 className='mb-[2.5rem] text-2xl md:text-4xl text-brand-secondary dark:text-slate-200 typography'>I'm a Software Engineer | FullStack Developer</h1>
-        <h4 className='mb-[1rem] text-lg mr-5'>I'm an enthusiastic Software engineer with multiple experiences on various web and mobile projects.</h4>
+        <h1 className='mb-[2.5rem] text-2xl md:text-4xl text-brand-secondary dark:text-slate-200 typography'>I&apos;m a Software Engineer | FullStack Developer</h1>
+        <h4 className='mb-[1rem] text-lg mr-5'>I&apos;m an enthusiastic Software engineer with multiple experiences on various web and mobile projects.</h4>
         <h5 className='text-sm md:text-xs text-brand-secondary dark:text-slate-200 typography text-gray-400'>I am currently seeking new job opportunities</h5>
       </div>
     </div>
